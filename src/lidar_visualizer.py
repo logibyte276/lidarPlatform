fec521829dbad3cbafb6220aff12fe1b699e16c0
Logibyte <109189988+logibyte276@users.noreply.github.com>
2026-08-08 18:31:51 -0700

    Update lidar_visualizer.py

--- src/lidar_visualizer.py
+++ src/lidar_visualizer.py
@@ -3,7 +3,7 @@ import numpy as np
 import open3d as o3d
 from lidar_udp_receiver import LidarStream, LidarUDPReceiver
 
-VIS_BLOCK = 17
+VIS_BLOCK = 5
 
 lidar = LidarStream()
 lidar.start()
@@ -15,14 +15,14 @@ pcd = o3d.geometry.PointCloud()
 vis.add_geometry(pcd)  
 vis.add_geometry(o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0, origin=[0, 0, 0]))
 ctr = vis.get_view_control()
-ctr.set_lookat([0, 0, 0])       # point the camera is aimed at
+ctr.set_lookat([0, 0, 1.5])       # point the camera is aimed at
 ctr.set_front([0, -1, 0.3])     # direction the camera faces, pointing toward lookat
 ctr.set_up([0, 0, 1])           # which way is "up" on screen
-ctr.set_zoom(0.5)               # smaller = zoomed in closer
+ctr.set_zoom(2)               # smaller = zoomed in closer
 
 try:
     while True:
-        scans = lidar.recent_scans(17)
+        scans = lidar.recent_scans(VIS_BLOCK)
         xyz_list = [s.xyz_intensity()[0] for s in scans]
         points = np.concatenate(xyz_list, axis=0).astype(np.float64)
 
@@ -32,7 +32,7 @@ try:
         if not vis.poll_events():   # False if the user closed the window
             break
         vis.update_renderer()
-        time.sleep(0.1)
+        time.sleep(0.01)
 
 except KeyboardInterrupt:
     print("\nStopped by Ctrl+C.")
